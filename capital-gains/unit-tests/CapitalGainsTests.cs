71156adad3e926f6d274f26de4847a7c924c8f10
Henrique Andrade Cruz <henrique.andrade22@hotmail.com>
2024-10-24 11:01:27 -0300

    add: unit tests for Program and CapitalGains

--- capital-gains/unit-tests/CapitalGainsTests.cs
+++ capital-gains/unit-tests/CapitalGainsTests.cs
@@ -5,30 +5,169 @@ namespace unit_tests;
 public class CapitalGainsTests
 {
     [Fact]
-    public void GetTaxes_GivenSingleBuyOperation_ReturnsZeroTax()
+    public void GetTaxes_GivenSimulation_ShouldReturnsRelatedTaxes()
     {
-        var simulation = new List<TradeOperation>
+        var capitalGains = new CapitalGains();
+        var simulations = new List<TradeOperation>
         {
-            new TradeOperation { Operation = "buy", UnitCost = 10.00m, Quantity = 100 }
+            new() { Operation = "buy", UnitCost = 10.00m, Quantity = 10000 },
+            new() { Operation = "sell", UnitCost = 20.00m, Quantity = 5000 },
+            new() { Operation = "sell", UnitCost = 0.00m, Quantity = 5000 }
         };
 
-        var taxes = CapitalGains.GetTaxes(simulation);
+        var taxes = capitalGains.GetTaxes(simulations);
 
-        Assert.Single(taxes);
-        Assert.Equal(0.00m, taxes.First().tax);
+        Assert.Equivalent(new { tax = 0.00m }, taxes[0]);
+        Assert.Equivalent(new { tax = 10000.00m }, taxes[1]);
+        Assert.Equivalent(new { tax = 0.00m }, taxes[2]);
     }
 
     [Fact]
-    public void GetTaxes_GivenSingleBuyOperation_ReturnsZeroTax()
+    public void GetBuyTax_WhenQuantityZero_ShouldResetValuesAndReturnTaxZero()
     {
-        var simulation = new List<TradeOperation>
+        var capitalGains = new CapitalGains();
+        var trade = new TradeOperation
         {
-            new TradeOperation { Operation = "buy", UnitCost = 10.00m, Quantity = 100 }
+            Operation = "buy",
+            UnitCost = 20.00m,
+            Quantity = 50
         };
 
-        var taxes = CapitalGains.GetTaxes(simulation);
+        var tax = capitalGains.GetBuyTax(trade);
 
-        Assert.Single(taxes);
-        Assert.Equal(0.00m, taxes.First().tax);
+        Assert.Equivalent(new { tax = 0.00m }, tax);
+    }
+
+    [Fact]
+    public void GetBuyTax_WhenQuantityNotZero_ShouldSetNewWeightedAveragePriceAndReturnTaxZero()
+    {
+        var capitalGains = new CapitalGains();
+        var trade = new TradeOperation
+        {
+            Operation = "buy",
+            UnitCost = 20.00m,
+            Quantity = 50
+        };
+
+        capitalGains.GetBuyTax(trade);
+        var tax = capitalGains.GetBuyTax(trade);
+
+        Assert.Equivalent(new { tax = 0.00m }, tax);
+    }
+
+    [Fact]
+    public void GetNewWeightedAveragePrice_GivenTradeOperation_ShouldReturnNewWeightedAveragePrice()
+    {
+        var capitalGains = new CapitalGains();
+        var trade = new TradeOperation
+        {
+            Operation = "buy",
+            UnitCost = 20.00m,
+            Quantity = 50
+        };
+
+        var newWeightedAveragePrice = capitalGains.GetNewWeightedAveragePrice(trade);
+
+        Assert.Equal(20.00m, newWeightedAveragePrice);
+    }
+
+    [Fact]
+    public void GetSellTax_WhenUnitCostEqualThanWeightedAveragePrice_ShouldReturnZeroTax()
+    {
+        var capitalGains = new CapitalGains();
+        var buy = new TradeOperation
+        {
+            Operation = "buy",
+            UnitCost = 10.00m,
+            Quantity = 100
+        };
+
+        capitalGains.GetBuyTax(buy);
+
+        var sell = new TradeOperation
+        {
+            Operation = "sell",
+            UnitCost = 15.00m,
+            Quantity = 50
+        };
+
+        var tax = capitalGains.GetSellTax(sell);
+
+        Assert.Equivalent(new { tax = 0.00m }, tax);
+    }
+
+    [Fact]
+    public void GetSellTax_WhenSaleValueLessOrEqualThan20000_ShouldReturnZeroTax()
+    {
+        var capitalGains = new CapitalGains();
+        var buy = new TradeOperation
+        {
+            Operation = "buy",
+            UnitCost = 10.00m,
+            Quantity = 100
+        };
+
+        capitalGains.GetBuyTax(buy);
+
+        var sell = new TradeOperation
+        {
+            Operation = "sell",
+            UnitCost = 15.00m,
+            Quantity = 50
+        };
+
+        var tax = capitalGains.GetSellTax(sell);
+
+        Assert.Equivalent(new { tax = 0.00m }, tax);
+    }
+
+    [Fact]
+    public void GetSellTax_WhenBalanceGreaterThanZero_ShouldReturnCalculatedTax()
+    {
+        var capitalGains = new CapitalGains();
+        var buy = new TradeOperation
+        {
+            Operation = "buy",
+            UnitCost = 10.00m,
+            Quantity = 10000
+        };
+
+        capitalGains.GetBuyTax(buy);
+
+        var sell = new TradeOperation
+        {
+            Operation = "sell",
+            UnitCost = 20.00m,
+            Quantity = 5000
+        };
+
+        var tax = capitalGains.GetSellTax(sell);
+
+        Assert.Equivalent(new { tax = 10000.00m }, tax);
+    }
+
+    [Fact]
+    public void GetSellTax_WhenUnitCostLessThanWeightedAveragePrice_ShouldReturnZeroTax()
+    {
+        var capitalGains = new CapitalGains();
+        var buy = new TradeOperation
+        {
+            Operation = "buy",
+            UnitCost = 10.00m,
+            Quantity = 10000
+        };
+
+        capitalGains.GetBuyTax(buy);
+
+        var sell = new TradeOperation
+        {
+            Operation = "sell",
+            UnitCost = 5.00m,
+            Quantity = 5000
+        };
+
+        var tax = capitalGains.GetSellTax(sell);
+
+        Assert.Equivalent(new { tax = 0.00m }, tax);
     }
 }
--- capital-gains/unit-tests/ProgramTests.cs
+++ capital-gains/unit-tests/ProgramTests.cs
@@ -4,8 +4,23 @@ namespace unit_tests;
 
 public class ProgramTests
 {
+    [Theory]
+    [MemberData(nameof(GetCasesData))]
+    public void Main_GivenCases_ShouldPrintOutputs(string input, string expected)
+    {
+        using var inputReader = new StringReader(input + "\n");
+        using var outputWriter = new StringWriter();
+        Console.SetIn(inputReader);
+        Console.SetOut(outputWriter);
+
+        Program.Main();
+
+        var output = outputWriter.ToString();
+        Assert.Contains(expected, output);
+    }
+
     [Fact]
-    public void GetSimulations_GivenSingleInput()
+    public void GetSimulations_GivenSingleInput_ReturnSimulations()
     {
         var input = "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 100}, {\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 50}]";
 
@@ -17,7 +32,7 @@ public class ProgramTests
     }
 
     [Fact]
-    public void GetSimulations_GivenMultipleInput()
+    public void GetSimulations_GivenMultipleInput_ReturnSimulations()
     {
         var input = "[[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 100}, {\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 50}],[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":20.00, \"quantity\": 5000}]]";
 
@@ -56,16 +71,53 @@ public class ProgramTests
     }
 
     [Fact]
-    public void Print_GivenOutput()
+    public void GetOutputs_GivenSimulations_ShouldReturnOutputs()
     {
+        var simulations = new List<List<TradeOperation>>
+        {
+            new()
+            {
+                new TradeOperation { Operation = "buy", UnitCost = 10.00m, Quantity = 100 },
+                new TradeOperation { Operation = "sell", UnitCost = 15.00m, Quantity = 50 }
+            },
+            new()
+            {
+                new TradeOperation { Operation = "buy", UnitCost = 10.00m, Quantity = 10000 },
+                new TradeOperation { Operation = "sell", UnitCost = 20.00m, Quantity = 5000 }
+            }
+        };
+
+        var outputs = Program.GetOutputs(simulations);
+
+        Assert.Equal(2, outputs.Count);
+
+        AssertFirstOutput(outputs[0]);
+        AssertSecondOutput(outputs[1]);
+    }
+
+    private static void AssertFirstOutput(List<object> outputs)
+    {
+        Assert.Equivalent(new { tax = 0.00m }, outputs[0]);
+        Assert.Equivalent(new { tax = 0.00m }, outputs[1]);
+    }
+
+    private static void AssertSecondOutput(List<object> outputs)
+    {
+        Assert.Equivalent(new { tax = 0.00 }, outputs[0]);
+        Assert.Equivalent(new { tax = 10000.00 }, outputs[1]);
+    }
+
+    [Fact]
+    public void Print_GivenOutput_ShouldPrintOutputs()
+    {
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+
         var output = new List<List<object>>
         {
             new() { new { tax = 0.00m }, new { tax = 0.00m } },
             new() { new { tax = 0.00m }, new { tax = 10000.00m } }
         };
-
-        using var writer = new StringWriter();
-        Console.SetOut(writer);
 
         Program.Print(output);
 
@@ -73,152 +125,60 @@ public class ProgramTests
         Assert.Equal(expected, writer.ToString());
     }
 
-
-
-
-
-
-    [Fact]
-    public void Main_Case1()
+    public static IEnumerable<object[]> GetCasesData()
     {
-        var input = "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 100}, {\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 50}, {\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 50}]";
+        yield return new object[]
+        {
+            "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 100}, {\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 50}, {\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 50}]",
+            "[{\"tax\":0.00},{\"tax\":0.00},{\"tax\":0.00}]"
+        };
 
-        using var inputReader = new StringReader(input + "\n");
-        using var outputWriter = new StringWriter();
-        Console.SetIn(inputReader);
-        Console.SetOut(outputWriter);
+        yield return new object[]
+        {
+            "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":20.00, \"quantity\": 5000}, {\"operation\":\"sell\", \"unit-cost\":5.00, \"quantity\": 5000}]",
+            "[{\"tax\":0.00},{\"tax\":10000.00},{\"tax\":0.00}]"
+        };
 
-        Program.Main();
+        yield return new object[]
+        {
+            "[[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 100}, {\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 50}, {\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 50}],[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":20.00, \"quantity\": 5000}, {\"operation\":\"sell\", \"unit-cost\":5.00, \"quantity\": 5000}]]",
+            "[{\"tax\":0.00},{\"tax\":0.00},{\"tax\":0.00}]\r\n[{\"tax\":0.00},{\"tax\":10000.00},{\"tax\":0.00}]"
+        };
 
-        var output = outputWriter.ToString();
-        Assert.Contains("[{\"tax\":0.00},{\"tax\":0.00},{\"tax\":0.00}]", output);
-    }
+        yield return new object[]
+        {
+            "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":5.00, \"quantity\": 5000}, {\"operation\":\"sell\", \"unit-cost\":20.00, \"quantity\": 3000}]",
+            "[{\"tax\":0.00},{\"tax\":0.00},{\"tax\":1000.00}]"
+        };
 
-    [Fact]
-    public void Main_Case2()
-    {
-        var input = "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":20.00, \"quantity\": 5000}, {\"operation\":\"sell\", \"unit-cost\":5.00, \"quantity\": 5000}]";
+        yield return new object[]
+        {
+            "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000}, {\"operation\":\"buy\", \"unit-cost\":25.00, \"quantity\": 5000}, {\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 10000}]",
+            "[{\"tax\":0.00},{\"tax\":0.00},{\"tax\":0.00}]"
+        };
 
-        using var inputReader = new StringReader(input + "\n");
-        using var outputWriter = new StringWriter();
-        Console.SetIn(inputReader);
-        Console.SetOut(outputWriter);
+        yield return new object[]
+        {
+            "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000}, {\"operation\":\"buy\", \"unit-cost\":25.00, \"quantity\": 5000}, {\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":25.00, \"quantity\": 5000}]",
+            "[{\"tax\":0.00},{\"tax\":0.00},{\"tax\":0.00},{\"tax\":10000.00}]"
+        };
 
-        Program.Main();
+        yield return new object[]
+        {
+            "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":2.00, \"quantity\": 5000}, {\"operation\":\"sell\", \"unit-cost\":20.00, \"quantity\": 2000}, {\"operation\":\"sell\", \"unit-cost\":20.00, \"quantity\": 2000}, {\"operation\":\"sell\", \"unit-cost\":25.00, \"quantity\": 1000}]",
+            "[{\"tax\":0.00},{\"tax\":0.00},{\"tax\":0.00},{\"tax\":0.00},{\"tax\":3000.00}]"
+        };
 
-        var output = outputWriter.ToString();
-        Assert.Contains("[{\"tax\":0.00},{\"tax\":10000.00},{\"tax\":0.00}]", output);
-    }
+        yield return new object[]
+        {
+            "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":2.00, \"quantity\": 5000}, {\"operation\":\"sell\", \"unit-cost\":20.00, \"quantity\": 2000}, {\"operation\":\"sell\", \"unit-cost\":20.00, \"quantity\": 2000}, {\"operation\":\"sell\", \"unit-cost\":25.00, \"quantity\": 1000}, {\"operation\":\"buy\", \"unit-cost\":20.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 5000}, {\"operation\":\"sell\", \"unit-cost\":30.00, \"quantity\": 4350}, {\"operation\":\"sell\", \"unit-cost\":30.00, \"quantity\": 650}]",
+            "[{\"tax\":0.00},{\"tax\":0.00},{\"tax\":0.00},{\"tax\":0.00},{\"tax\":3000.00},{\"tax\":0.00},{\"tax\":0.00},{\"tax\":3700.00},{\"tax\":0.00}]"
+        };
 
-    [Fact]
-    public void Main_Case1And2()
-    {
-        var input = "[[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 100}, {\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 50}, {\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 50}],[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":20.00, \"quantity\": 5000}, {\"operation\":\"sell\", \"unit-cost\":5.00, \"quantity\": 5000}]]";
-
-        using var inputReader = new StringReader(input + "\n");
-        using var outputWriter = new StringWriter();
-        Console.SetIn(inputReader);
-        Console.SetOut(outputWriter);
-
-        Program.Main();
-
-        var output = outputWriter.ToString();
-        Assert.Contains("[{\"tax\":0.00},{\"tax\":0.00},{\"tax\":0.00}]\r\n[{\"tax\":0.00},{\"tax\":10000.00},{\"tax\":0.00}]", output);
-    }
-
-    [Fact]
-    public void Main_Case3()
-    {
-        var input = "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":5.00, \"quantity\": 5000}, {\"operation\":\"sell\", \"unit-cost\":20.00, \"quantity\": 3000}]";
-
-        using var inputReader = new StringReader(input + "\n");
-        using var outputWriter = new StringWriter();
-        Console.SetIn(inputReader);
-        Console.SetOut(outputWriter);
-
-        Program.Main();
-
-        var output = outputWriter.ToString();
-        Assert.Contains("[{\"tax\":0.00},{\"tax\":0.00},{\"tax\":1000.00}]", output);
-    }
-
-    [Fact]
-    public void Main_Case4()
-    {
-        var input = "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000}, {\"operation\":\"buy\", \"unit-cost\":25.00, \"quantity\": 5000}, {\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 10000}]";
-
-        using var inputReader = new StringReader(input + "\n");
-        using var outputWriter = new StringWriter();
-        Console.SetIn(inputReader);
-        Console.SetOut(outputWriter);
-
-        Program.Main();
-
-        var output = outputWriter.ToString();
-        Assert.Contains("[{\"tax\":0.00},{\"tax\":0.00},{\"tax\":0.00}]", output);
-    }
-
-    [Fact]
-    public void Main_Case5()
-    {
-        var input = "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000}, {\"operation\":\"buy\", \"unit-cost\":25.00, \"quantity\": 5000}, {\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":25.00, \"quantity\": 5000}]";
-
-        using var inputReader = new StringReader(input + "\n");
-        using var outputWriter = new StringWriter();
-        Console.SetIn(inputReader);
-        Console.SetOut(outputWriter);
-
-        Program.Main();
-
-        var output = outputWriter.ToString();
-        Assert.Contains("[{\"tax\":0.00},{\"tax\":0.00},{\"tax\":0.00},{\"tax\":10000.00}]", output);
-    }
-
-    [Fact]
-    public void Main_Case6()
-    {
-        var input = "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":2.00, \"quantity\": 5000}, {\"operation\":\"sell\", \"unit-cost\":20.00, \"quantity\": 2000}, {\"operation\":\"sell\", \"unit-cost\":20.00, \"quantity\": 2000}, {\"operation\":\"sell\", \"unit-cost\":25.00, \"quantity\": 1000}]";
-
-        using var inputReader = new StringReader(input + "\n");
-        using var outputWriter = new StringWriter();
-        Console.SetIn(inputReader);
-        Console.SetOut(outputWriter);
-
-        Program.Main();
-
-        var output = outputWriter.ToString();
-        Assert.Contains("[{\"tax\":0.00},{\"tax\":0.00},{\"tax\":0.00},{\"tax\":0.00},{\"tax\":3000.00}]", output);
-    }
-
-    [Fact]
-    public void Main_Case7()
-    {
-        var input = "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":2.00, \"quantity\": 5000}, {\"operation\":\"sell\", \"unit-cost\":20.00, \"quantity\": 2000}, {\"operation\":\"sell\", \"unit-cost\":20.00, \"quantity\": 2000}, {\"operation\":\"sell\", \"unit-cost\":25.00, \"quantity\": 1000}, {\"operation\":\"buy\", \"unit-cost\":20.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":15.00, \"quantity\": 5000}, {\"operation\":\"sell\", \"unit-cost\":30.00, \"quantity\": 4350}, {\"operation\":\"sell\", \"unit-cost\":30.00, \"quantity\": 650}]";
-
-        using var inputReader = new StringReader(input + "\n");
-        using var outputWriter = new StringWriter();
-        Console.SetIn(inputReader);
-        Console.SetOut(outputWriter);
-
-        Program.Main();
-
-        var output = outputWriter.ToString();
-        Assert.Contains("[{\"tax\":0.00},{\"tax\":0.00},{\"tax\":0.00},{\"tax\":0.00},{\"tax\":3000.00},{\"tax\":0.00},{\"tax\":0.00},{\"tax\":3700.00},{\"tax\":0.00}]", output);
-    }
-
-    [Fact]
-    public void Main_Case8()
-    {
-        var input = "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":50.00, \"quantity\": 10000}, {\"operation\":\"buy\", \"unit-cost\":20.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":50.00, \"quantity\": 10000}]";
-
-        using var inputReader = new StringReader(input + "\n");
-        using var outputWriter = new StringWriter();
-        Console.SetIn(inputReader);
-        Console.SetOut(outputWriter);
-
-        Program.Main();
-
-        var output = outputWriter.ToString();
-        Assert.Contains("[{\"tax\":0.00},{\"tax\":80000.00},{\"tax\":0.00},{\"tax\":60000.00}]", output);
+        yield return new object[]
+        {
+            "[{\"operation\":\"buy\", \"unit-cost\":10.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":50.00, \"quantity\": 10000}, {\"operation\":\"buy\", \"unit-cost\":20.00, \"quantity\": 10000}, {\"operation\":\"sell\", \"unit-cost\":50.00, \"quantity\": 10000}]",
+            "[{\"tax\":0.00},{\"tax\":80000.00},{\"tax\":0.00},{\"tax\":60000.00}]"
+        };
     }
 }
