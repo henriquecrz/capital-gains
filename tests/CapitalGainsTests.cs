35949b9b77be302334179156694acccd9295abd3
Henrique Andrade <henrique.andrade22@hotmail.com>
2024-11-21 18:20:55 -0300

    add: unit tests

--- tests/CapitalGainsTests.cs
+++ tests/CapitalGainsTests.cs
@@ -23,6 +23,28 @@ public class CapitalGainsTests
     }
 
     [Fact]
+    public void GetTaxes_GivenBlockAccountSimulation_ShouldReturnsAccountBlocked()
+    {
+        var capitalGains = new CapitalGains();
+        var simulations = new List<TradeOperation>
+        {
+            new() { Operation = "sell", UnitCost = 20.00m, Quantity = 10000 },
+            new() { Operation = "sell", UnitCost = 20.00m, Quantity = 10000 },
+            new() { Operation = "sell", UnitCost = 20.00m, Quantity = 10000 },
+            new() { Operation = "buy", UnitCost = 10.00m, Quantity = 10000 },
+            new() { Operation = "sell", UnitCost = 20.00m, Quantity = 10000 }
+        };
+
+        var taxes = capitalGains.GetTaxes(simulations);
+
+        Assert.Equivalent(new { error = "Can't sell more stocks than you have" }, taxes[0]);
+        Assert.Equivalent(new { error = "Can't sell more stocks than you have" }, taxes[1]);
+        Assert.Equivalent(new { error = "Can't sell more stocks than you have" }, taxes[2]);
+        Assert.Equivalent(new { error = "Your account is blocked" }, taxes[3]);
+        Assert.Equivalent(new { error = "Your account is blocked" }, taxes[4]);
+    }
+
+    [Fact]
     public void GetBuyTax_WhenQuantityZero_ShouldResetValuesAndReturnTaxZero()
     {
         var capitalGains = new CapitalGains();
@@ -92,7 +114,7 @@ public class CapitalGainsTests
             Quantity = 50
         };
 
-        var tax = capitalGains.GetSellTax(sell);
+        var tax = capitalGains.GetSellOutput(sell);
 
         Assert.Equivalent(new { tax = 0.00m }, tax);
     }
@@ -117,7 +139,7 @@ public class CapitalGainsTests
             Quantity = 50
         };
 
-        var tax = capitalGains.GetSellTax(sell);
+        var tax = capitalGains.GetSellOutput(sell);
 
         Assert.Equivalent(new { tax = 0.00m }, tax);
     }
@@ -142,7 +164,7 @@ public class CapitalGainsTests
             Quantity = 5000
         };
 
-        var tax = capitalGains.GetSellTax(sell);
+        var tax = capitalGains.GetSellOutput(sell);
 
         Assert.Equivalent(new { tax = 10000.00m }, tax);
     }
@@ -167,8 +189,56 @@ public class CapitalGainsTests
             Quantity = 5000
         };
 
-        var tax = capitalGains.GetSellTax(sell);
+        var tax = capitalGains.GetSellOutput(sell);
 
         Assert.Equivalent(new { tax = 0.00m }, tax);
+    }
+
+    [Fact]
+    public void GetSellTax_WhenTradeQuantityGreaterThanExistingQuantity_ShouldReturnError()
+    {
+        //var capitalGains = new CapitalGains();
+        //var buy = new TradeOperation
+        //{
+        //    Operation = "buy",
+        //    UnitCost = 20.00m,
+        //    Quantity = 10000
+        //};
+
+        //capitalGains.GetBuyTax(buy);
+
+        //var sell = new TradeOperation
+        //{
+        //    Operation = "sell",
+        //    UnitCost = 2000m,
+        //    Quantity = 11000
+        //};
+
+        //var error = capitalGains.GetSellOutput(sell);
+
+        //Assert.Equivalent(new { error = "Can't sell more stocks than you have" }, error);
+    }
+
+    [Fact]
+    public void HasEnoughQuantity_WhenTradeQuantityGreaterThanExistingQuantity_ShouldReturn()
+    {
+        var capitalGains = new CapitalGains();
+        var buy = new TradeOperation
+        {
+            Operation = "buy",
+            UnitCost = 20.00m,
+            Quantity = 10000
+        };
+        capitalGains.GetBuyTax(buy);
+
+        var sell = new TradeOperation
+        {
+            Operation = "sell",
+            UnitCost = 2000m,
+            Quantity = 11000
+        };
+        var hasEnoughQuantity = capitalGains.HasEnoughQuantity(sell);
+
+        Assert.False(hasEnoughQuantity);
     }
 }
